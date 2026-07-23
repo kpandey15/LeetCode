1f789ef8000af4a5ede7bc2b4025ff9a938f785d
Kohina Pandey <89478207+kpandey15@users.noreply.github.com>
2026-07-23 12:10:22 +0530

    Time: 54 ms (5.39%), Space: 47.9 MB (33.05%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -12,6 +12,7 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         if(grid[0][0] == 1)
             return -1;
+
         int[][] dist = new int[grid.length][grid[0].length];
         PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x,y) -> x.dis - y.dis);
 
@@ -31,6 +32,7 @@ class Solution {
 
             if(i == grid.length-1 && j == grid[0].length-1)
                 break;
+
             if(i > 0){
                 if(dist[i-1][j] > dis+1 && grid[i-1][j] != 1){
                     dist[i-1][j] = dis+1;
@@ -46,7 +48,7 @@ class Solution {
 
                 if(j <= grid[0].length-2){
                     if(dist[i-1][j+1] > dis+1+1 && grid[i-1][j+1] != 1){
-                        dist[i-1][j+1] = dis+1+1;
+                        dist[i-1][j+1] = dis+1;
                         pq.add(new Pair(dis+1 , i-1 , j+1));
                     }
                 }
